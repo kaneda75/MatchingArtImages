4cc94349c705cbdef3e54269f0ff390edf8cd635
Xavi Escriche <xescriche@hotmail.com>
2012-09-16 19:41:30 +0200

    Primera subida 2

---
+++ formulari.cpp
@@ -0,0 +1,15 @@
+/*
+ * File:   formulari.cpp
+ * Author: xescriche
+ *
+ * Created on 16 de septiembre de 2012, 16:34
+ */
+
+#include "formulari.h"
+
+formulari::formulari() {
+    widget.setupUi(this);
+}
+
+formulari::~formulari() {
+}

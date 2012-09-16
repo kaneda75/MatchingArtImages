a0fbbc4c11d67d94ca5d1ff40fc5db4705e3bb12
Xavi Escriche <xescriche@hotmail.com>
2012-09-16 19:34:42 +0200

    Primera subida

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * File:   main.cpp
+ * Author: xescriche
+ *
+ * Created on 16 de septiembre de 2012, 19:29
+ */
+
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
